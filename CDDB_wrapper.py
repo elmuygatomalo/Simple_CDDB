97d532e1a5530d4e1024967a7b3619221fb7c846
brizer <brizer.st.cyr@gmail.com>
2012-12-19 15:45:46 -0800

    Implemented adding entry.

--- CDDB_wrapper.py
+++ CDDB_wrapper.py
@@ -2,12 +2,12 @@
 #adding entries
 #removing entries
 #querying entries
-<<<<<<< HEAD
+#<<<<<<< HEAD
 import sys
 import _mysql
 import submit_entry
 import db_query
-
+import MySQLdb as msb
 
 
 musicDB_name=""
@@ -15,62 +15,75 @@ musicDB_name_query=""
 musicDB=None
 
 menu="What would you like to do? \n"
-menu=menu+"1-Add Somthing?"
-menu=menu+"2-Remove Somethign?"
-menu=menu+"3-Search for something?"
-menu=menu+"4-Get the fuck out"
+menu=menu+"1-Add tracks by cd key \n"
+menu=menu+"2-Remove Somethign \n"
+menu=menu+"3-Search for something \n"
+menu=menu+"4-Get the fuck out "
 #read config file to determine if any previous database was used
 #check for existing config file
 #check for database 
 
 #musicDB_name=howeverifindthatout
+#if musicDB != None:#
+#	musicDB_name_query="Would you like to use "+musicDB_name+" as your 
+#music database?: "
+#	answer = raw_input(musicDB_name_query)
+#	answer.lower()
+#	
+#	if answer=="n" or answer=="no":#
+#		musicDB_name=raw_input("Enter new database name: ")
+#else:
+#	musicDB_name=raw_input("Enter Database name: ")
+#	
+#	
+#hostname=raw_input("Please enter hostname: ")
+#username=raw_input("Please enter username: ")
+#password=raw_input("Please enter password: ")
 
-musicDB_name_query="Would you like to use "+musicDB_name+" as your music database?: "
-answer = raw_input(musicDB_name_query)
-answer.lower()
 
-if answer=="n" or answer=="no":
-	musicDB_name=raw_input("Enter new database name: ")
+musicDB=None
+username=None
+password=None
+musicDB_name="musicdb"
 
-hostname=raw_input("Please enter hostname: ")
-username=raw_input("Please enter username: ")
-password=raw_input("Please enter password: ")
+username=raw_input("Please Enter Username: ")
+password=raw_input("Please Enter Password: ")
 
 
-con=None
-
-while con==None:
+while musicDB==None:
 	try:
-		musicDB = _mysql.connect(hostname,username,password, musicDB_name)
+		musicDB = msb.connect("localhost",username,password, musicDB_name)
 	except _mysql.Error,e:
-		print "You fucked up"
+		print "You fucked up you login credentials, if they even exist you jerk"
+		sys.exit()
 
-musicDB.execute("CREATE TABLE IF NOT EXISTS Artists(Artist STRING, Album VARCHAR(25), Song VARCHAR(25), DiscID VARCHAR(8))")
-musicDB.execute("CREATE TABLE IF NOT EXISTS Albums(Artist STRING, Album VARCHAR(25), Song VARCHAR(25), DiscID VARCHAR(8))")
-musicDB.execute("CREATE TABLE IF NOT EXISTS Songs(Artist STRING, Album VARCHAR(25), Song VARCHAR(25), DiscID VARCHAR(8))")
+cur=musicDB.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS Artists ( Id int PRIMARY KEY AUTO_INCREMENT, Artist varchar(25), Album varchar(25), Song varchar(25), DiscID varchar(8));")
+cur.execute("CREATE TABLE IF NOT EXISTS Albums ( Id int PRIMARY KEY AUTO_INCREMENT, Album varchar(25), Artist varchar(25), Song varchar(25), DiscID varchar(8));")
+cur.execute("CREATE TABLE IF NOT EXISTS Songs ( Id int PRIMARY KEY AUTO_INCREMENT, Song varchar(25), Artist varchar(25), Album varchar(25), DiscID varchar(8));")
 
-                
-while true:
+
+while True :
 	print menu
 	answer=raw_input("\n:")
 	if answer=="1":
-		#call adding function
-		print "this is 1"
-	elif anser=="2":
+		cd_key=raw_input("Enter 10 digit CD: ")
+		submit_entry.add_entry(cur,cd_key,type="DiscID")
+	elif answer=="2":
 		#call removal funciton
 		print "this is also 1"
-	elif anser=="3":
+	elif answer=="3":
 		#call search function
 		print "this too is 1"
-	elif anser=="4":
+	elif answer=="4":
 		print "Later Motherfucker"
 		break
 	else:
 		print "Wrong Answer"
 
-con.close()
+musicDB.close()
 
 
 
-=======
->>>>>>> 3d2463222eb5145137b5b17bf080c01579ca2fdc
+#=======
+#>>>>>>> 3d2463222eb5145137b5b17bf080c01579ca2fdc
--- submit_entry.py
+++ submit_entry.py
@@ -3,10 +3,10 @@
 import _mysql
 import string
 
-def add_entry(*args, **entry_type):
+def add_entry(database,*args, **entry_type):
 	#DiscID: The only argument provided is the Disc
 	#This will use the DiscID to lookup all tracks on disc and add each artist, song and album to the respective DBS
-	if options.get("type")  == "DiscID":
+	if entry_type.get("type")  == "DiscID":
 		#Query the Net to find the Album name/Artists/song names relating to discid
 		DiscID=args[0]
 		#Some Crazy TCP shit I'll do later
@@ -15,20 +15,24 @@ def add_entry(*args, **entry_type):
 		album_name="IMAALBUM"
 		artist_name="IMAARTIST"
 		for index in xrange(tracks):
-			#Insert
-			sql_insert_string="INSERT INTO Songs VALUES("+artist_name+","+album_name+","+song_name+","+DiscID
+			#Insert track into Songs database
+			sql_insert_string="insert into Songs (Artist,Album,Song,DiscID) values('"+artist_name+"','"+album_name+"','"+song_name+"','"+DiscID+"');"
+			print(sql_insert_string)
 			try:
-				musicDB.execute(sql_insert_string)
+				database.execute(sql_insert_string)
+			except _mysql.Error, e:
+				print "You fucked up\n"
+				print e
+			#Insert track into Albums database
+			sql_insert_string="insert into Albums (Song,Artist,Album,DiscID) values('"+artist_name+"','"+album_name+"','"+song_name+"','"+DiscID+"');"
+			try:
+				database.execute(sql_insert_string)
 			except _mysql.Error, e:
 				print "You fucked up"
-			sql_insert_string="INSERT INTO Artists VALUES("+artist_name+","+album_name+","+song_name+","+DiscID
+			#Insert track into Artists database
+			sql_insert_string="insert into Artists (Song,Artist,Album,DiscID) values('"+artist_name+"','"+album_name+"','"+song_name+"','"+DiscID+"');"
 			try:
-				musicDB.execute(sql_insert_string)
-			except _mysql.Error, e:
-				print "You fucked up"
-			sql_insert_string="INSERT INTO Albums VALUES("+artist_name+","+album_name+","+song_name+","+DiscID
-			try:
-				musicDB.execute(sql_insert_string)
+				database.execute(sql_insert_string)
 			except _mysql.Error, e:
 				print "You fucked up"
 		return
